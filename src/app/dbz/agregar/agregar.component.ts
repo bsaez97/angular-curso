76d1a3202839686fa27568741b731fd1f537d599
bsaez97 <bsaez.ki@gmail.com>
2021-09-11 19:59:15 -0300

    se agregan modificacione a dbzService

--- src/app/dbz/agregar/agregar.component.ts
+++ src/app/dbz/agregar/agregar.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Personaje } from '../interfaces/dbz.interface';
+
+import { DbzService } from '../services/dbz.service';
 
 @Component({
   selector: 'app-agregar',
@@ -14,17 +16,19 @@ export class AgregarComponent {
     poder: 0
   };
 
-  @Output() onNewCharacter: EventEmitter<Personaje> = new EventEmitter();
+  constructor( private dbzService: DbzService ) {}
+
+  // @Output() onNewCharacter: EventEmitter<Personaje> = new EventEmitter();
 
   agregar() {
-    if ( this.nuevo.nombre.trim().length === 0 ) {
-      return;
-    }
+    if ( this.nuevo.nombre.trim().length === 0 ) { return; }
 
-    console.log(this.nuevo);
+    // console.log(this.nuevo);
 
     // this.personajes.push(this.nuevo);
-    this.onNewCharacter.emit( this.nuevo );
+    // this.onNewCharacter.emit( this.nuevo );
+
+    this.dbzService.agregarPersonaje( this.nuevo );
 
     this.nuevo = {
       nombre: '',
--- src/app/dbz/services/dbz.service.ts
+++ src/app/dbz/services/dbz.service.ts
@@ -19,8 +19,17 @@ export class DbzService {
         return [...this._personajes];
     };
     
+
+    // SE PODRIA ELIMINAR PERO LO MANTENDREMOS PARA LA CLARIDAD DEL CURSO
+
     constructor() {
-        console.log('Servicio inicializado');
+        // console.log('Servicio inicializado');
+    }
+
+    // METODOS SEGUN DOCUMENTACION DEBERIA ESTAR SIEMPRE BAJO EL CONSTRUCTOR DE EXISTIR
+
+    agregarPersonaje( personaje: Personaje ) {
+        this._personajes.push( personaje );
     }
 
 }
